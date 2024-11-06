99e4d30b82d9c2d345e5395289263a63aaf4ecb2
Tinus66 <martijn.de.jong4@hva.nl>
2024-11-06 08:59:16 +0100

    Update Samen1.py

--- Samen1.py
+++ Samen1.py
@@ -450,8 +450,7 @@ if selected == 'Luchthavens':
         st.plotly_chart(fig)
 
 # Aanroepen van de slider grafiek
-    if st.checkbox("Toon interactieve grafiek met slider"):
-        create_aircraft_slider_plot()
+
 
 #--------------------------------------------------------------------------------------
 # Filterfunctie voor jaar
